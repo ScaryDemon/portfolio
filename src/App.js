288fe5e1967487f17cc164f4222f834e6f2e7b7b
mel <ravendocx@protonmail.com>
2020-01-13 15:30:10 +0100

    added landing page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import {Link} from  'react-router-dom';
 
 function App() {
   return (
-    <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
+    <div>
+    <Layout>
+        <Header class="header-color" title="Mel Raeven" scroll>
             <Navigation>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
@@ -25,10 +25,11 @@ function App() {
             </Navigation>
         </Drawer>
         <Content>
-          <Main/>
+            <Main/>
+            <div className="page-content" />
         </Content>
     </Layout>
-  </div>
+</div>
   );
 }
 
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import '../App.css';
+import Particles from 'react-particles-js';
+import Logo from '../assets/images/logo.png';
+
 
 class Landing extends Component {
     render(){ 
         return(
-        <div><h1>landing page</h1></div>
+        <div class="landingPage">
+            <img src={Logo} class="logo-image"></img>
+            <Particles 
+            id="simple"
+            width="100%"
+            height="100vh"
+            style={{
+                backgroundColor: '#1a1c1b',
+            }}
+            params={{
+                particles: {
+                    number: {
+                        value: 100,
+                    },
+                    size: {
+                        value: 5,
+                    },
+                },
+                interactivity: {
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: 'repulse',
+                        },
+                    },
+                },
+            }}/>
+        </div>
         )
     }
 }
