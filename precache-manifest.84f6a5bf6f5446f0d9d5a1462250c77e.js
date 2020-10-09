97bec8a5545b3931186daccd58460e7714e76be2
Mel Raeven <melraeven@MacBook-Pro-van-Mel.local>
2020-10-09 11:14:19 +0200

    Updates

---
+++ precache-manifest.84f6a5bf6f5446f0d9d5a1462250c77e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c619b475b7d99acb1b9185660728ee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "21817ee2e00579c3f4cd",
+    "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "116adb7abf4f44d79b84",
+    "url": "/portfolio/static/css/main.35ae2aa0.chunk.css"
+  },
+  {
+    "revision": "21817ee2e00579c3f4cd",
+    "url": "/portfolio/static/js/2.472be37e.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/portfolio/static/js/2.472be37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116adb7abf4f44d79b84",
+    "url": "/portfolio/static/js/main.680bb4f7.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "3309ca1330ca91b55b4feeda3f383031",
+    "url": "/portfolio/static/media/background.3309ca13.jpg"
+  },
+  {
+    "revision": "9d8d053a59be2ddaf9f7a8b09b36894c",
+    "url": "/portfolio/static/media/logo.9d8d053a.png"
+  }
+]);
